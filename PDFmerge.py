c80d8f9c0e42bac0bb576219e8c7f244e0d0fffb
welshypie <welshmattd@gmail.com>
2021-03-13 21:54:05 -0600

    created list_pdfs function

--- PDFmerge.py
+++ PDFmerge.py
@@ -1,4 +1,6 @@
 from PyPDF2 import PdfFileMerger
+import os
+import re
 
 usrInput = ''
 pdfs = []
@@ -14,3 +16,16 @@ for pdf in pdfs:
 
 merger.write("output.pdf")
 merger.close()
+
+
+def list_pdfs():
+	'''
+	This function should return a list all PDFs in the current directory.
+	'''
+	dircontents = os.listdir(os.getcwd())
+	pdflist = []
+	for i in dircontents:
+		if i[-4:] == ".pdf":
+			pdflist.append(i)
+			
+	return pdflist
